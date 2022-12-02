97f94a2ed0958ca1f11b8849c1a15207ade633b0
mamena2020 <mamenaware751@gmail.com>
2022-12-02 19:22:03 +0900

    fix config prod

--- src/config/config.js
+++ src/config/config.js
@@ -1,22 +1,12 @@
-// const configurationPeerConnection = {
-//     sdpSemantics: "unified-plan",
-//     iceServers: [{
-//             "urls": "stun:stun.stunprotocol.org"
-//         },
-
-//     ]
-// }
 async function configurationPeerConnection(){
 
-    var allowTurnServer = process.env.ALLOW_TURN_SERVER =="true" ||process.env.ALLOW_TURN_SERVER ==true?true:false   
+      
 
     // var stun2 = {"urls": "stun:stun.l.google.com:19302"}
 
-    return {
-        sdpSemantics: "unified-plan",
-        iceServers: [
+   var  iceServers= [
             {"urls": "stun:stun.stunprotocol.org"},
-             {
+            {
                 "urls": "stun:openrelay.metered.ca:80",
                 "username": "openrelayproject",
                 "credential": "openrelayproject",
@@ -46,83 +36,33 @@ async function configurationPeerConnection(){
                 "username": "openrelayproject",
                 "credential": "openrelayproject",
             },
-        ]
-    }
+        ] 
 
-    var iceServers = [];
-
-    iceServers.push(stun1)
-    // iceServers.push(stun2)
-
-    if(allowTurnServer)
+    try
     {
-        console.log("using turn server")
-
-        var turnServerHost = process.env.TURN_SERVER_HOST
-        var turnServerUsername = process.env.TURN_SERVER_USERNAME
-        var turnServerPassword = process.env.TURN_SERVER_PASSWORD
-
-        if(turnServerHost!="" && turnServerHost.length>2)
-        {
-            var turn =  {
-                            'urls': turnServerHost,
-                            'username': turnServerUsername,
-                            'password': turnServerPassword,
-                        }
-        //    iceServers.push(turn)
-           console.log("add aditional turn server")
-        }
-
-        var turnServersDefault = [
-             
-                // //---------------------------- open relay
-                // {
-                //     "urls": "stun:openrelay.metered.ca:80",
-                //     "username": "openrelayproject",
-                //     "credential": "openrelayproject",
-                // },
-                // {
-                //     "urls": "turn:openrelay.metered.ca:80",
-                //     "username": "openrelayproject",
-                //     "credential": "openrelayproject",
-                // },
-                // {
-                //     "urls": "turn:openrelay.metered.ca:443",
-                //     "username": "openrelayproject",
-                //     "credential": "openrelayproject",
-                // },
-                // {
-                //     "urls": "turn:openrelay.metered.ca:80?transport=tcp",
-                //     "username": "openrelayproject",
-                //     "credential": "openrelayproject",
-                // },
-                // {
-                //     "urls": "turn:openrelay.metered.ca:443?transport=tcp",
-                //     "username": "openrelayproject",
-                //     "credential": "openrelayproject",
-                // },
-                {
-                    "urls": "turns:openrelay.metered.ca:443",
-                    "username": "openrelayproject",
-                    "credential": "openrelayproject",
-                },
-        ]
-        iceServers.push(turnServersDefault)
-        
-        return {
-            sdpSemantics: "unified-plan",
-            iceServers: iceServers
-        }
-    }
-    else
+            var allowTurnServer = process.env.ALLOW_TURN_SERVER =="true" ||process.env.ALLOW_TURN_SERVER ==true?true:false 
+            var turnServerHost = process.env.TURN_SERVER_HOST
+            var turnServerUsername = process.env.TURN_SERVER_USERNAME
+            var turnServerPassword = process.env.TURN_SERVER_PASSWORD
+            if(turnServerHost!="" && turnServerHost.length>3 && allowTurnServer==true)
+            {
+                var turn =  {
+                                'urls': turnServerHost,
+                                'username': turnServerUsername,
+                                'password': turnServerPassword,
+                            }
+                iceServers.push(turn)
+                console.log("add aditional turn server")
+            }   
+    }catch(e)
     {
-
-        return {
-            sdpSemantics: "unified-plan",
-            iceServers: iceServers
-        }
+        console.log(e)
     }
-   
+
+    return {
+        sdpSemantics: "unified-plan",
+        iceServers: iceServers
+    }
 }
 
 const offerSdpConstraints = {
